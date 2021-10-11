ab3edc7945517e22d57c65ccff9d74d926855bf9
mikemei <meixu@huawei.com>
2021-10-11 17:32:41 +0800

    initial commit

---
+++ app/src/main/java/com/huawei/hms/ads/vast/demo/Constants.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+ */
+
+package com.huawei.hms.ads.vast.demo;
+
+public class Constants {
+
+    public static final String LINEAR_SLOT_ID = "testy3cglm3pj0";
+
+    public static final String APP_NAME = "ppsservicedemoSdv";
+
+    public static final boolean IS_TEST = false;
+
+    public static final int WHITE_COLOR = 0xFFFFFFFF;
+
+    public static final int BLUE_COLOR = 0xFF2962FF;
+
+    public static final int BLACK_COLOR = 0xFF000000;
+
+    public static final int GRAY_COLOR = 0xFF666666;
+
+    public static final int CORNER_RADIUS = 10;
+
+    public static final float TIP_TEXT_SIZE = 8;
+
+    public static final float DESC_TEXT_SIZE = 10;
+
+    public static final float DETAIL_TEXT_SIZE = 12;
+
+    public static final float TITLE_TEXT_SIZE = 12;
+
+    public static final int TOTAL_DURATION = 99;
+
+    public static final int LINEAR_WIDTH_SIZE = 400;
+
+    public static final int LINEAR_HEIGHT_SIZE = 300;
+}
